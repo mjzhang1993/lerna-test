2c7b9b661d229a5afe5d05db9dccf8f85754b4d7
mjzhang1993 <mingjiazhang@yufuid.com>
2020-12-22 16:54:16 +0800

    5 commit

--- packages/lerna-package-2/lib/lerna-package-2.js
+++ packages/lerna-package-2/lib/lerna-package-2.js
@@ -5,7 +5,7 @@ const lernaPackage1 = require('@mjz-test/lerna-package-1');
 module.exports = lernaPackage2;
 
 function lernaPackage2() {
-    console.log(lernaPackage1('this is str'));
+    lernaPackage1('this is str');
 }
 
 console.log('this is running in package 2');
