ab65eaf6c76008e01756266a8c64cac2d845cd15
mjzhang1993 <mingjiazhang@yufuid.com>
2020-12-23 19:33:36 +0800

    fix(core): 修复 core package-2 中的问题
    affects: @mjz-test/lerna-package-2
    
    ISSUES CLOSED: #123

--- packages/lerna-package-2/lib/lerna-package-2.js
+++ packages/lerna-package-2/lib/lerna-package-2.js
@@ -5,7 +5,7 @@ const lernaPackage1 = require('@mjz-test/lerna-package-1');
 module.exports = lernaPackage2;
 
 function lernaPackage2() {
-    lernaPackage1('this is strs');
+    lernaPackage1('this is strsss');
 }
 
 console.log('this is running in package 2');
